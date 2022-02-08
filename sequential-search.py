3a4494734fd488f695c0922ef4be3260dd2b9c3c
Park Jun Woo <97827368+pjnw1236@users.noreply.github.com>
2022-02-08 23:06:11 +0900

    Update sequential-search.py

--- sequential-search.py
+++ sequential-search.py
@@ -9,17 +9,17 @@
 
 # array에서 value 값이 존재하면, 해당 value 값이 속해 있는 array의 index 반환/ 값이 존재하지 않다면 -1 반환
 
-def search_list(array, value):
+def search_value(array, value):
     n = len(array) 
     for i in range(n):
         if value == array[i]:
             return i
-    return -1 
+    return None 
 
 
 # 다음 예시로 확인
-v = [17, 92, 18, 33, 58, 7, 33, 42]
+v = [1, 3, 5, 7, 9]
 
-print(search_list(v, 18))  # 2
-print(search_list(v, 33))  # 3
-print(search_list(v, 900)) # -1
+print(search_value(v, 5)) # 2
+print(search_value(v, 7)) # 3
+print(search_value(v, 2)) # None
